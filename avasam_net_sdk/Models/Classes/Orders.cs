4a8917432671dc4f68bf6765bf111b82f254abec
Bhaveshs <Bhaveshs@EBG-38>
2019-08-16 19:30:46 +0530

    changes for get order for current login supplier

--- avasam_net_sdk/Models/Classes/Orders.cs
+++ avasam_net_sdk/Models/Classes/Orders.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 
 namespace avasam_net_sdk.Models.Classes
 {
-   public class Orders : BaseContext
+    public class Orders : BaseContext
     {
+
         public Orders(LoginResponse apiContext) : base(apiContext)
         {
         }
@@ -14,11 +15,21 @@ namespace avasam_net_sdk.Models.Classes
         // Search Order By Order Id
         public async Task<List<DropShipperOrders>> GetOrders(GetOrdersRequest value)
         {
-            return await Post<List<DropShipperOrders>>("api/ProductSDK/GetShipperOrderList", (new GetOrdersRequest()
+            return await Post<List<DropShipperOrders>>("api/SellerOrdersView/SellerGetOrdersListWithFilter", (new SellerGetOrdersListWithFilterRequest()
             {
-                supplier = value.supplier,
-                OrderNumber = value.OrderNumber,
-                limit = value.limit,
+                Customer = string.IsNullOrWhiteSpace(value.OrderNumber) ? "All" : value.OrderNumber,
+                CustomerId = string.IsNullOrWhiteSpace(value.OrderNumber) ? "All" : value.OrderNumber,
+                Date = null,
+                DateDispaly = "Last 7 days",
+                FromDate = DateTime.UtcNow.AddDays(-3).ToString("yyyy-MM-ddTHH:mm:ss"),
+                IsToday = "false",
+                OrderStatus = "All",
+                PaymentStatus = "All",
+                SortStatus = "down",
+                Sortby = "CustomerName",
+                ToDate = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss"),
+                customerId = "All",
+                limit = value.limit.ToString(),
                 page = value.page
             }).ToJson());
         }
@@ -46,16 +57,16 @@ namespace avasam_net_sdk.Models.Classes
         {
             return await Post<UpdateResp>("api/OrdersCreation/CreateNewOrder", (new OrderViewModel()
             {
-              BillingAddress = value.BillingAddress,
-              CustomerID = value.CustomerID,
-              CustomerInfo = value.CustomerInfo,
-              CustomerName = value.CustomerName, 
-              CustomerType =  value.CustomerType,
-              GeneralInfo = value.GeneralInfo, 
-              Items = value.Items,
-              Notes = value.Notes, 
-              ShippingInfo = value.ShippingInfo,
-              ShippingService = value.ShippingService
+                BillingAddress = value.BillingAddress,
+                CustomerID = value.CustomerID,
+                CustomerInfo = value.CustomerInfo,
+                CustomerName = value.CustomerName,
+                CustomerType = value.CustomerType,
+                GeneralInfo = value.GeneralInfo,
+                Items = value.Items,
+                Notes = value.Notes,
+                ShippingInfo = value.ShippingInfo,
+                ShippingService = value.ShippingService
             }).ToJson());
         }
 
@@ -68,14 +79,14 @@ namespace avasam_net_sdk.Models.Classes
         {
             return await Post<UpdateResp>("api/OrderDispatch/OrderDispatchProcess", (new DispatchProcess()
             {
-              _id = value._id,
-              CustomerId = value.CustomerId,
-              TrackingNumber = value.TrackingNumber,
-              ShippingMethod =  value.ShippingMethod,
-              PostalServiceName = value.PostalServiceName,
-              NumOrderId = value.NumOrderId,
-              CustomerOrderId = value.CustomerOrderId,
-              OrderId =  value.OrderId, 
+                _id = value._id,
+                CustomerId = value.CustomerId,
+                TrackingNumber = value.TrackingNumber,
+                ShippingMethod = value.ShippingMethod,
+                PostalServiceName = value.PostalServiceName,
+                NumOrderId = value.NumOrderId,
+                CustomerOrderId = value.CustomerOrderId,
+                OrderId = value.OrderId,
             }).ToJson());
         }
 
---
+++ avasam_net_sdk/Models/Classes/Orders/SellerGetOrdersListWithFilterRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace avasam_net_sdk.Models.Classes
+{
+    public class SellerGetOrdersListWithFilterRequest
+    {
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string DateDispaly { get; set; }
+        public string IsToday { get; set; }
+        public object Date { get; set; }
+        public string Customer { get; set; }
+        public string OrderStatus { get; set; }
+        public string PaymentStatus { get; set; }
+        public string Sortby { get; set; }
+        public string SortStatus { get; set; }
+        public string limit { get; set; }
+        public string customerId { get; set; }
+        public int page { get; set; }
+        public string CustomerId { get; set; }
+        public string lToDate { get; set; }
+    }
+}
